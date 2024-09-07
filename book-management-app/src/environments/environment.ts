eb6c38a50ed44d762e872a989eca704106eea47b
johljohl <joohan.lindell@gmail.com>
2024-09-07 08:25:59 +0200

    Added a point

--- book-management-app/src/environments/environment.ts
+++ book-management-app/src/environments/environment.ts
@@ -1,12 +1,6 @@
 // environment.ts
 export const environment = {
-  production: false,
-  apiUrl: 'https://bookmanager-lh64.onrender.com/api'  // Korrekt backend-URL
-};
-
-
- /* export const environment = {
-    production: true,
-    apiUrl: 'https://bookmanager-lh64.onrender.com/api'  // Detta ska vara din backend-URL
+    production: false,
+    apiUrl: 'https://localhost:5004' // Ensure this matches your backend API URL
   };
-  */
+  
